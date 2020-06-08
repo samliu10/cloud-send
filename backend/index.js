e4ee4046b2b0c5753ff3bf7daf11424d9d467fa4
Samantha Liu <sl2486@cornell.edu>
2020-06-08 11:01:12 -0400

    more get/post requests, changed db channel setup

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 /*
+
 Backend routes for Cloud-Chat, a Slack-like messaging web application.
 Database is hosted by Google Firebase.
 
-Date last modified: June 2, 2020
+Date last modified: June 8, 2020
 
 */
 
@@ -21,6 +22,10 @@ const db = admin.firestore();
 const app = express();
 const port = 8080;
 app.use(bodyParser.json());
+
+const channelsTable = db.collection("channels");
+const messagesTable = db.collection("messages");
+const usersTables = db.collection("users");
 
 // returns array of documents. doc.data() returns json
 const getMessagesHelper = (channel, array, doc_list) => {
@@ -44,8 +49,7 @@ io.on('connection', (client) => {
         console.log('Message received', data);
 
         // store that message in db
-        const channel_name = data.channel;
-        new_message = db.collection(channel_name).doc();
+        new_message = messagesTable.doc();
         new_message.set(data);
 
         // emits data back to all users on chat channel
@@ -56,24 +60,41 @@ io.on('connection', (client) => {
 
 // get all channel names
 app.get('/getallchannels/', async (req, res) => {
-    const channels = await db.collection("channels").get();
-    res.status(200).json({ channels: channels });
+    const channels = await channelsTable.get();
+    res.status(200).json({ channels });
 })
 
 // get all users
 app.get('/getallusers/', async (req, res) => {
-    const users = await db.collection("users").get();
-    res.status(200).json({ users: users });
+    const users = await usersTables.get();
+    res.status(200).json({ users });
 })
 
 // get all messages in a channel 
 app.get('/getmessages/:channel', async (req, res) => {
     const channel = req.params.channel;
-    const messages = await db.collection("messages").orderBy("time").get();
+    const messages = await messagesTable.orderBy("time").get();
     const array = [];
     res.status(200).json(getMessagesHelper(channel, array, messages).map(
         (item) => ({ ...item.data })
     ));
+})
+
+// get channel description
+app.get('/getchanneldescription/:channelname', async (req, res) => {
+    const channel_name = req.params.channelname;
+    const channel = await channelsTable.get(channel_name);
+    const description = channel.description;
+    res.status(200).json({ description });
+})
+
+// make new channel
+app.post('/makenewchannel/', async (req, res) => {
+    const channel_name = req.body.channel_name;
+    const description = req.body.description;
+    const new_channel = channelsTable.doc(channel_name);
+    new_channel.set({ description });
+    res.status(201).json({ channel: new_channel });
 })
 
 server.listen(port, () => console.log(`Listening on port ${port}!`));
