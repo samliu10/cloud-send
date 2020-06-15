ce66d926e2e880b46b855ca665ed81818b5b3fb2
Samantha Liu <sl2486@cornell.edu>
2020-06-15 11:18:07 -0400

    dashboard props to navbar and channel

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,2 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Authentication from './Authentication';
+import Channel from './Channel';
+import NavBar from './NavBar';
+
+const [currChannel, setCurrChannel] = useState("");
+
+const changeChannel = (newChannel) => {
+    setCurrChannel(newChannel);
+}
+
+export default () => {
+    return (
+        <div>
+            <NavBar currChannel={currChannel} changeChannel={changeChannel} />
+            <Channel currChannel={currChannel} changeChannel={changeChannel} />
+
+        </div>
+    )
+}
